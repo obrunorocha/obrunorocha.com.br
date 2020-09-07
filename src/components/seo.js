0d3e6ef68a69c1f28a938c63b1d0fd7a48a93a13
Bruno Rocha <obrunorocha@outlook.com>
2020-09-07 16:05:03 -0300

    update seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -18,6 +18,7 @@ function SEO({ description, lang, meta, title, image }) {
       }
     `
   )
+  const author = site.siteMetadata.author
 
   const metaDescription = description || site.siteMetadata.description
   
@@ -34,8 +35,36 @@ function SEO({ description, lang, meta, title, image }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: `abstract`,
+          content: `Desenvolvedor Front-end Bruno Rocha Sorocaba Tutoriais Programador Web Sorocaba Otimização de Site Sorocaba Posicionamento no Google Sorocaba`,
+        },
+        {
+          name: `keywords`,
+          content: `Desenvolvedor Front-end Bruno Rocha Sorocaba Tutoriais Programador Web Sorocaba Otimização de Site Sorocaba Posicionamento no Google Sorocaba`,
+        },
+        {
+          name: `dc.keywords`,
+          content: `Desenvolvedor Front-end Bruno Rocha Sorocaba Tutoriais Programador Web Sorocaba Otimização de Site Sorocaba Posicionamento no Google Sorocaba`,
+        },
+        {
+          name: `author`,
+          content: `Bruno Rocha - Desenvolvedor Front-end Otimazação de Sites`,
+        },
+        {
           name: `description`,
           content: metaDescription,
+        },
+        {
+          name: `geo.placename`,
+          content: `Sorocaba, Sorocaba - SP, Brasil`,
+        },
+        {
+          name: `geo.region`,
+          content: `BR-São Paulo`,
+        },
+        {
+          property: `og:site_name`,
+          content: `Bruno Rocha - Desenvolvedor Front-End`,
         },
         {
           property: `og:title`,
@@ -54,6 +83,10 @@ function SEO({ description, lang, meta, title, image }) {
           content: ogImage
         },
         {
+          property: `og:url`,
+          content: url
+        },
+        {
           name: `twitter:card`,
           content: `summary_large_image`,
         },
@@ -63,7 +96,7 @@ function SEO({ description, lang, meta, title, image }) {
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: author,
         },
         {
           name: `twitter:title`,
@@ -73,13 +106,62 @@ function SEO({ description, lang, meta, title, image }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `contactCity`,
+          content: `Sorocaba`,
+        },
+        {
+          name: `robots`,
+          content: `all`,
+        },
+        {
+          name: `googlebot`,
+          content: `all`,
+        },
+        {
+          name: `InktomiSlurp`,
+          content: `all`,
+        },
+        {
+          name: `Unknownrobot`,
+          content: `all`,
+        },
+        {
+          name: `distribution`,
+          content: `global`,
+        },
+        {
+          name: `rating`,
+          content: `general`,
+        },
+        {
+          name: `revisit-after`,
+          content: `7 days`,
+        },
+        {
+          name: `email`,
+          content: `obrunorocha@outlook.com`,
+        },
+        {
+          name: `HandheldFriendly`,
+          content: `True`,
+        },
+        {
+          name: `MobileOptimized`,
+          content: `768`,
+        },
+        {
+          name: `apple-mobile-web-app-capable`,
+          content: `yes`,
+        },
+        
       ].concat(meta)}
     />
   )
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `pt-br`,
   meta: [],
   description: ``,
 }
